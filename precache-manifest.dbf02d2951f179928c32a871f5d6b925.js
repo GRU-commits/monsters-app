ca737925e5783108e01319bf3c907f7a8dd6e6ef
Zeyad Bakas <zeyadbakas@gmail.com>
2020-04-18 23:57:12 +0200

    Updates

---
+++ precache-manifest.dbf02d2951f179928c32a871f5d6b925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24be5e94c9f380c4672450d3611e7356",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "a9c82ccff5b14906ef99",
+    "url": "/monsters-app/static/css/main.435cc2de.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-app/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c82ccff5b14906ef99",
+    "url": "/monsters-app/static/js/main.05e461b6.chunk.js"
+  },
+  {
+    "revision": "254fc56a4548c1840f48",
+    "url": "/monsters-app/static/js/runtime-main.dff89ef1.js"
+  }
+]);
